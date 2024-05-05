5231bced80b1c72f95d22a1056c6d7ea00ed9cf7
trungnguyenw4 <w1795554@westminster.ac.uk>
2024-05-05 14:35:42 +0700

    create new xcode project

---
+++ What'sNews/What_sNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_sNewsApp.swift
+//  What'sNews
+//
+//  Created by Trung Nguyen on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct What_sNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
